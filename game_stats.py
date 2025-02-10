693d9bd3e4936b0227ac98ceeabaf54e498426b1
Othmane_laptop <oelmo031@uottawa.ca>
2025-02-10 01:31:38 -0500

    Create button class

--- game_stats.py
+++ game_stats.py
@@ -5,8 +5,8 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
-        #start alien invasion in active state
-        self.game_active = True
+        #start alien invasion in inaactive state
+        self.game_active = False
 
     def reset_stats(self):
         """initializa stats that can change during the game"""
